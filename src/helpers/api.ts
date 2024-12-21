0bf7f3190d0307ad0255553df6281d028a37f693
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2024-12-21 15:22:43 -0300

    Fallback base url to localhost.

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -2,9 +2,9 @@ import {create} from 'apisauce';
 
 // define the api
 export const usersApi = create({
-  baseURL: import.meta.env.VITE_USERS_API_BASE_URL,
+  baseURL: import.meta.env.VITE_USERS_API_BASE_URL ?? "http://localhost/api",
 });
 
 export const feedApi = create({
-  baseURL: import.meta.env.VITE_FEED_API_BASE_URL,
+  baseURL: import.meta.env.VITE_FEED_API_BASE_URL ?? "http://localhost/api",
 });
